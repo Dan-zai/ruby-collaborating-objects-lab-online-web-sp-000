c4851053ae19e8f910dc4755ab58e5645baa2b13
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-19 06:28:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,9 @@ class Song
 
   def self.new_by_filename(filename)
     
-    song = Song.new(filename.split(" - ")[1]) #creates new file by splitting input and adding song name to [1] array eg drake - nonstop = @@all[0] - [nonstop]
+    song = Song.new(filename.split(" - ")[1]) #creates new song from passed filename by splitting input and returning song name from split [1] array eg drake - nonstop now = nonstop
     
-    song.artist = Artist.new(filename.split(" - ")[0])
+    song.artist = Artist.new(filename.split(" - ")[0]) #defines song artist from passed filename by splitting input and returning artist name from split [0] array eg drake - nonstop now = nonstop
     
     song
     
