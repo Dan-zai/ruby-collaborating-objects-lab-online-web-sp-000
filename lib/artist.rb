300e08b7810d447fc69cd6bc6e916a3ba69add3e
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-19 06:23:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,4 +19,18 @@ class Artist
     song.artist = self
   end
   
-  
+  def self.find_or_create_by_name(name)
+    
+    matching_artists = self.all.select {|artist| artist.name == name}
+    if (matching_artists.length == 0)
+      Artist.new(name)
+    else
+      matching_artists[0]
+    end
+  end
+
+  def print_songs
+    self.songs.each do |song| song
+      puts song.name
+    end
+  end
